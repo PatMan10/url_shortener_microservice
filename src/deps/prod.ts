18046f2707358885da88544c23885ae596288e5d
PatMan10 <PatMan10@protonmail.com>
2022-01-24 20:31:18 +0200

    added deps to use dejs templating

--- src/deps/prod.ts
+++ src/deps/prod.ts
@@ -1,6 +1,10 @@
+// deno std lib
+export { dirname, join } from "https://deno.land/std@0.122.0/path/mod.ts";
 // opine web server
 export * from "https://deno.land/x/opine@2.0.2/mod.ts";
 // https status codes
 export * from "https://deno.land/x/https_status_codes@v1.1.0/mod.ts";
 // cors
 export { opineCors as cors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+// ejs template engine for deno
+export { renderFileToString } from "https://deno.land/x/dejs@0.10.2/mod.ts";
